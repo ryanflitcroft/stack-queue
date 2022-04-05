19b526d888a9b882aff37f64ba2ddefd802fd0f0
Ryan Flitcroft <ryanflitcroft@ymail.com>
2022-04-05 12:36:24 -0700

    conditional check for stack length on class Stack method pop. get size property written.

--- challenges/Queue.js
+++ challenges/Queue.js
@@ -24,17 +24,17 @@ export default class Queue {
   }
 }
 
-const queue = new Queue();
-queue.enqueue('fox');
-queue.enqueue('goose');
-queue.enqueue('lizard');
-console.log(queue.dequeue()); // 'fox'
-console.log(queue.hasNext()); // true
-console.log(queue.dequeue()); // 'goose'
-queue.enqueue('llama');
-console.log(queue.dequeue()); // 'lizard'
-console.log(queue.hasNext()); // true
-console.log(queue.dequeue()); // 'llama'
-console.log(queue.hasNext()); // false
-console.log(queue.dequeue()); // null
-console.log(queue.size);
+// const queue = new Queue();
+// queue.enqueue('fox');
+// queue.enqueue('goose');
+// queue.enqueue('lizard');
+// console.log(queue.dequeue()); // 'fox'
+// console.log(queue.hasNext()); // true
+// console.log(queue.dequeue()); // 'goose'
+// queue.enqueue('llama');
+// console.log(queue.dequeue()); // 'lizard'
+// console.log(queue.hasNext()); // true
+// console.log(queue.dequeue()); // 'llama'
+// console.log(queue.hasNext()); // false
+// console.log(queue.dequeue()); // null
+// console.log(queue.size);
--- challenges/Stack.js
+++ challenges/Stack.js
@@ -8,25 +8,30 @@ export default class Stack {
   }
 
   pop() {
-    return this.stack.pop();
+    return this.stack.length ? this.stack.pop() : null;
   }
 
   peek() {
     return this.stack[this.stack.length - 1];
   }
+
+  get size() {
+    return this.stack.length;
+  }
 }
 
-// const stack = new Stack();
-// stack.push('fox');
-// stack.push('goose');
-// stack.push('lizard');
+const stack = new Stack();
+stack.push('fox');
+stack.push('goose');
+stack.push('lizard');
 
-// console.log(stack);
-// console.log(stack.pop());
-// console.log(stack.peek());
-// console.log(stack.pop());
-// stack.push('llama');
-// console.log(stack.pop());
-// console.log(stack.peek());
-// console.log(stack.pop());
-// console.log(stack.pop());
+console.log(stack);
+console.log(stack.pop());
+console.log(stack.peek());
+console.log(stack.pop());
+stack.push('llama');
+console.log(stack.pop());
+console.log(stack.peek());
+console.log(stack.pop());
+console.log(stack.pop());
+console.log(stack.size);
