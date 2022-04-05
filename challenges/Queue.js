6ebb21a4378e715654eea8fac07f49f76b83c4dc
Ryan Flitcroft <ryanflitcroft@ymail.com>
2022-04-05 12:24:58 -0700

    refactor class Queue instance methods dequeue, hasNext.

--- challenges/Queue.js
+++ challenges/Queue.js
@@ -8,26 +8,24 @@ export default class Queue {
   }
 
   dequeue() {
-    if (this.hasNext()) {
-      return this.queue.shift();
-    } else return null;
+    return this.queue.length ? this.queue.shift() : null;
   }
 
   hasNext() {
-    return this.queue.length !== 0;
+    return !!this.queue.length;
   }
 }
 
-// const queue = new Queue();
-// queue.enqueue('fox');
-// queue.enqueue('goose');
-// queue.enqueue('lizard');
-// console.log(queue.dequeue()); // 'fox'
-// console.log(queue.hasNext()); // true
-// console.log(queue.dequeue()); // 'goose'
-// queue.enqueue('llama');
-// console.log(queue.dequeue()); // 'lizard'
-// console.log(queue.hasNext()); // true
-// console.log(queue.dequeue()); // 'llama'
-// console.log(queue.hasNext()); // false
-// console.log(queue.dequeue()); // null
+const queue = new Queue();
+queue.enqueue('fox');
+queue.enqueue('goose');
+queue.enqueue('lizard');
+console.log(queue.dequeue()); // 'fox'
+console.log(queue.hasNext()); // true
+console.log(queue.dequeue()); // 'goose'
+queue.enqueue('llama');
+console.log(queue.dequeue()); // 'lizard'
+console.log(queue.hasNext()); // true
+console.log(queue.dequeue()); // 'llama'
+console.log(queue.hasNext()); // false
+console.log(queue.dequeue()); // null
