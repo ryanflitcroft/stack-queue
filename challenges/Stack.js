0498a8c45a444d93c5ae2b75fb1544108e69dec0
Ryan Flitcroft <ryanflitcroft@ymail.com>
2022-04-05 10:48:00 -0700

    Class stack, instance methods push, pop, peek written.

---
+++ challenges/Stack.js
@@ -0,0 +1,32 @@
+export default class Stack {
+  constructor() {
+    this.stack = [];
+  }
+
+  push(item) {
+    return this.stack.push(item);
+  }
+
+  pop() {
+    return this.stack.pop();
+  }
+
+  peek() {
+    return this.stack[this.stack.length - 1];
+  }
+}
+
+const stack = new Stack();
+stack.push('fox');
+stack.push('goose');
+stack.push('lizard');
+
+console.log(stack);
+console.log(stack.pop());
+console.log(stack.peek());
+console.log(stack.pop());
+stack.push('llama');
+console.log(stack.pop());
+console.log(stack.peek());
+console.log(stack.pop());
+console.log(stack.pop());
