1df47e5f46302609758e90c50e55779d92162584
Ryan Flitcroft <ryanflitcroft@ymail.com>
2022-04-05 17:58:36 -0700

    class PersonTreeNode method findPErson started, not quite right.

--- challenges/PersonTreeNode.js
+++ challenges/PersonTreeNode.js
@@ -16,6 +16,32 @@ export default class PersonTreeNode {
     }
   }
 
+  findPerson(name) {
+    // takes in a name string
+    // checks if name === this.value (person.name)
+    // if so: return this.person (the whole obj)
+    // if not: is name < or > this.value
+
+    // if less than (this.left)
+    // repeat: this.left.findPerson(name)
+    // else null
+
+    // if greater than (this.right)
+    // repeat: this.right.findPerson(name)
+    // else null
+
+    if (name === this.value) {
+      return this.person;
+    } 
+    if (name < this.value) {
+        this.left.findPerson(name);
+      } else {
+        if (name > this.value) {
+          this.right.findPerson(name);
+        }
+      }
+    }
+  }
   //
 }
 
@@ -25,4 +51,14 @@ const person1 = new PersonTreeNode({
   address: '123 Naito',
 });
 
+const person2 = new PersonTreeNode({
+  name: 'Carrey',
+  phone: '83292829',
+  address: 'ndu3h3u',
+});
+
+person1.add(person2);
+
 console.log(person1);
+console.log(person1.findPerson('Nelson'));
+console.log(person1.findPerson('Carrey'));
