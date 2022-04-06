0cb82ad2abf7f8d886041cf96092b59b5650b880
Ryan Flitcroft <ryanflitcroft@ymail.com>
2022-04-05 17:11:13 -0700

    Class BinaryTreeNode, method add written.

---
+++ challenges/BinaryNodeTree.js
@@ -0,0 +1,34 @@
+export default class BinaryTreeNode {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+
+  add(node) {
+    // is node < or > this.value?
+    // if  < check this.left === null
+    // if true, this.left = node
+    //if > check this.right === null
+    // if true, this.right = node
+    // if false, this.'side'.add(node)
+
+    if (node.value < this.value) {
+      if (!this.left) this.left = node;
+      else this.left.add(node);
+    } else {
+      if (!this.right) this.right = node;
+      else this.right.add(node);
+    }
+  }
+}
+
+const B = new BinaryTreeNode('B');
+const A = new BinaryTreeNode('A');
+const C = new BinaryTreeNode('C');
+const D = new BinaryTreeNode('D');
+
+// B will be the root of the tree:
+B.add(A);
+B.add(D);
+B.add(C);
